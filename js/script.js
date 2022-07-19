d53163fce9a3d16affecaf3cf913d2c240c1ca1e
vusalsalman <77886090+vusalsalman@users.noreply.github.com>
2022-07-19 15:09:43 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//Init the carousel
+initSlider();
+
+function initSlider() {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    onInitialized: startProgressBar,
+    onTranslate: resetProgressBar,
+    onTranslated: startProgressBar
+  });
+}
+
+function startProgressBar() {
+  // apply keyframe animation
+  $(".slide-progress").css({
+    width: "100%",
+    transition: "width 5000ms"
+  });
+}
+
+function resetProgressBar() {
+  $(".slide-progress").css({
+    width: 0,
+    transition: "width 0s"
+  });
+}
